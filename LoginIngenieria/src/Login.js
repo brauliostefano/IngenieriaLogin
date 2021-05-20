c4890fe8547e484ddd4fe6b5fa58a2a6925490f0
LI Javier Santiago <li_s.javier@hotmail.com>
2021-05-19 23:59:52 -0500

    Agregando validacion de email vacio

--- LoginIngenieria/src/Login.js
+++ LoginIngenieria/src/Login.js
@@ -10,138 +10,64 @@ function Login() {
 
   // Buscar los intentos del email en la colleccion "user_auth_fails"
   const getUserFails = async() => {
-    if(email.length > 0){
-      const res = await db.collection("user_auth_fails").doc(email)
-          .get()
-      if(res){
-        return res.data();
-      }
+    const res = await db.collection("user_auth_fails").doc(email)
+        .get()
+    if(res){
+      return res.data();
     }
     return null;
   }
 
   // Actualizar los intentos del email en la colleccion "user_auth_fails"
   const setUserFails = async(email,failsNumber) => {
-    if(email.length > 0){
-      await db.collection("user_auth_fails").doc(email)
-          .set({
-            fails: failsNumber,
-          })
-    }
+    await db.collection("user_auth_fails").doc(email)
+        .set({
+          fails: failsNumber,
+        })
   }
   
   const login = async(event) => {
     event.preventDefault();
     // Obtener numero intentos anteriores
-    let userFails = await getUserFails();
-    if(userFails == undefined || userFails.fails < 3){
-      auth
-        .signInWithEmailAndPassword(email, password)
-        .then((auth) => {
-          history.push("/");
-        })
-        .catch((e) => {
-          // Aumentar el contador del correo electronico del campo fails
-          setUserFails(email,userFails == undefined ? 1 : userFails.fails + 1);
-          if (
-            e.message ===
-            "La contraseña es invalida o el usuario no ingresó la contraseña correcta."
-          ) {
-            alert("Por favor ingresa tus datos nuevamente.");
-          } else if (
-            e.message ===
-            "No hay un usuario asociado a esta cuenta o la cuenta ha sido eliminada."
-          ) {
-            alert("Por favor ingresa tus datos nuevamente.");
-          } else {
-            alert(e.message);
-          } 
-          alert(
+    if(email.length > 0){
+      alert('Ingrese un correo electrónico')
+    }else{
+
+      let userFails = await getUserFails();
+      if(userFails == undefined || userFails.fails < 3){
+        auth
+          .signInWithEmailAndPassword(email, password)
+          .then((auth) => {
+            history.push("/");
+          })
+          .catch((e) => {
+            // Aumentar el contador del correo electronico del campo fails
+            setUserFails(email,userFails == undefined ? 1 : userFails.fails + 1);
+            if (
+              e.message ===
               "La contraseña es invalida o el usuario no ingresó la contraseña correcta."
-          );
-        });
-      }else{
-        alert("Cuenta inhabilitada.");
+            ) {
+              alert("Por favor ingresa tus datos nuevamente.");
+            } else if (
+              e.message ===
+              "No hay un usuario asociado a esta cuenta o la cuenta ha sido eliminada."
+            ) {
+              alert("Por favor ingresa tus datos nuevamente.");
+            } else {
+              alert(e.message);
+            } 
+            alert(
+                "La contraseña es invalida o el usuario no ingresó la contraseña correcta."
+            );
+          });
+        }else{
+          alert("Cuenta inhabilitada.");
+        }
       }
     }
 
     return (
       <div className="login">
-        <img src="" className="login__logo" alt="" />
-        <div className="login__container">
-          <h3>We&Co</h3>
-          <form>
-            <center>
-              <input
-                type="email"
-                placeholder="correo electronico"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-            </center>
-            <center>
-              <input
-                type="password"
-                placeholder="contraseña"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </center>
-            <center>
-              <button onClick={login} type="submit" className="login__login">
-                iniciar sesion
-              </button>
-            </center>
-            <center>
-              <div className="sideinfo">
-                <h5>¿olvidaste tu contraseña?</h5>
-                <h5 className="dot">.</h5>
-                <Link to="/register" style={{ textDecoration: "none" }}>
-                  <h5 className="rtd">registrate</h5>
-                </Link>
-              </div>
-            </center>
-          </form>
-        </div>
-      </div>
-    );
-  }
-  
-    // Obtener numero intentos anteriores
-    let userFails = await getUserFails();
-    if(userFails == undefined || userFails.fails < 3){
-      auth
-        .signInWithEmailAndPassword(email, password)
-        .then((auth) => {
-          history.push("/");
-        })
-        .catch((e) => {
-          // Aumentar el contador del correo electronico del campo fails
-          setUserFails(email,userFails == undefined ? 1 : userFails.fails + 1);
-          if (
-            e.message ===
-            "La contraseña es invalida o el usuario no ingresó la contraseña correcta."
-          ) {
-            alert("Por favor ingresa tus datos nuevamente.");
-          } else if (
-            e.message ===
-            "No hay un usuario asociado a esta cuenta o la cuenta ha sido eliminada."
-          ) {
-            alert("Por favor ingresa tus datos nuevamente.");
-          } else {
-            alert(e.message);
-          } 
-          alert(
-              "La contraseña es invalida o el usuario no ingresó la contraseña correcta."
-          );
-        });
-      }else{
-        alert("Cuenta inhabilitada.");
-      }
-    }
-
-  return (
-    <div className="login">
       <img src="" className="login__logo" alt="" />
       <div className="login__container">
         <h3>We&Co</h3>
@@ -151,24 +77,18 @@ function Login() {
               required
               type="email"
               placeholder="correo electronico"
-<<<<<<< HEAD
-              required onChange={(e) => setEmail(e.target.value)}
-=======
+              required
               onChange={(e) => setEmail(e.target.value)}
               value={email}
->>>>>>> 9dc30a330cc8dfd3cc5a99f82a2213094d4159c2
             />
           </center>
           <center>
             <input
               type="password"
               placeholder="contraseña"
-<<<<<<< HEAD
-              required onChange={(e) => setPassword(e.target.value)}
-=======
+              required
               onChange={(e) => setPassword(e.target.value)}
               value={password}
->>>>>>> 9dc30a330cc8dfd3cc5a99f82a2213094d4159c2
             />
           </center>
           <center>
@@ -188,7 +108,7 @@ function Login() {
         </form>
       </div>
     </div>
-  );
+    );
 }
 
 export default Login;
